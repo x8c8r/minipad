bef36b258ab69d3b883498717cd524d6efae2356
Fl1p <newfl1pnatic@gmail.com>
2022-03-23 20:43:59 +0600

    hate

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
-  <nEditor/>
+<div>
+    <h1>miniPad</h1>
+    <textarea rows="25" class="text_editor" placeholder="Enter text here..." v-model="content"></textarea>
+    <button @click.prevent="save_file" class="save_b">Save</button>
+</div>
 </template>
 
 <script>
-import nEditor from './nEditor.vue'
+import saveAs from 'file-saver'
 
 export default {
-  name: 'App',
-  components: {
-    nEditor
-  }
+    data() {
+        return {
+            title: 'New File',
+            content: '',
+        }
+    },
+
+    methods: {      
+        save_file() {
+            if(this.content.replace(/\s+/g, '') != '') {
+                var blob = new Blob([this.content], {type: "text/plain;charset=utf-8"})
+                this.file_name = prompt("Enter file name:", this.title + ".txt")
+                if(this.file_name.replace(/\s+/g, '') != '')
+                    saveAs(blob, this.file_name)
+            }
+        }
+    }
 }
 </script>
 
 <style>
+.text_editor {
+    width: 100%;
+    background-color: #131313;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    margin-top: 5px;
+    white-space: pre-wrap;
+}
+.save_b {
+    background-color: #101010;
+    color: #ffffff;
+    border: none;
+    padding: 10px;
+    border-radius: 5px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    font-size: 150%;
+    cursor: pointer;
+    width: 200px;
+}
 html {
   background-color: #181818;
 }
